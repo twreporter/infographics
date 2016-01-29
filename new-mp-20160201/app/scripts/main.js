5aaa3f4a97e17dadb85d2056d40c337abfc1af33
garfieldduck <garfieldduck@gmail.com>
2016-01-29 14:30:11 +0800

    added the second slide & integrated velocity animation

--- new-mp-20160201/app/scripts/main.js
+++ new-mp-20160201/app/scripts/main.js
@@ -16,6 +16,16 @@ function setActiveBars(activeCounts) {
 function playSlide0() {
   console.log('playSlide0');
   $('#progress-nav').hide();
+
+  $('#yuan-welcome').velocity(
+    {'opacity': [1, 0, 'linear'], 'margin-top': ['0', '100px']},
+    { delay: 0, duration: 1000 }
+  );
+
+  $('.description-box').velocity(
+    {'opacity': [1, 0, 'linear'], 'left': ['50%', '80%']},
+    { delay: 0, duration: 1500 }
+  );
 }
 
 function playSlide1() {
@@ -38,6 +48,26 @@ function playSlide4() {
   setActiveBars(4);
 }
 
+// function showSlide1Dialog(message) {
+//   $('.twr-quick-view').css({
+// 	    "top": topSelected, // this is the selected image top value
+// 	    "left": leftSelected, // this is the selected image left value
+// 	    "width": widthSelected, // this is the selected image width
+// 	}).velocity({
+// 		//animate the quick view: animate its width and center it in the viewport
+// 		//during this animation, only the slider image is visible
+// 	    'width': sliderFinalWidth+'px',
+// 	    'left': finalLeft+'px', // ($(window).width - sliderFinalWidth)/2,
+// 	    'top': finalTop+ 'px', // ($(window).height - slider final height)/2,
+// 	}, 1000, [ 400, 20 ])
+// 	.velocity({
+// 		'width': quickViewWidth+'px', // 80% of the viewport
+// 		'left': quickViewLeft+'px', // 10% of the viewport
+// 	}, 300, 'ease', function(){
+// 		//show quick view content
+// 		$('.twr-quick-view').addClass('add-content');
+// 	}).addClass('is-visible');
+// }
 
 $( document ).ready(function() {
   var slideTarget = window.location.hash.replace(/#/, '');
