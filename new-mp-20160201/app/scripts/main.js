9de247172973442a38234f8978b2ded21ae2b061
garfieldduck <garfieldduck@gmail.com>
2016-01-30 14:21:08 +0800

    finalize slide 2

--- new-mp-20160201/app/scripts/main.js
+++ new-mp-20160201/app/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let sParty = null;
+
 function setActiveBars(activeCounts) {
   // controls the progress bar on the top
   $('#progress-nav').show();
@@ -74,16 +76,37 @@ function showSlide1Dialog(btn, message) {
 	});
 }
 
-function showSlide2Dialog(btn, message) {
+function showSlide2Dialog(btn, party) {
+  switch(party) {
+    case 'B':
+      $('#slide2-quickview .title').html('<p>所有藍黨立委已經在上週開過黨團會議，會中決議支持藍黨的賴小葆為立法院長。</p>');
+      $('#slide2-quickview .middle-pic').attr({src: '/images/decision-blue.svg'});
+      sParty = 'B';
+      break;
+    case 'Y':
+      $('#slide2-quickview .title').html('<p>所有黃黨立委已經在上週開過黨團會議，由於黃黨無人表達角逐院長意願，會中決議一致支持綠黨的蘇小全為立法院長。</p>');
+      $('#slide2-quickview .middle-pic').attr({src: '/images/decision-green.svg'});
+      sParty = 'Y';
+      break;
+    case 'G':
+      $('#slide2-quickview .title').html('<p>所有綠黨立委已經在上週開過黨團會議，會中決議支持綠黨的蘇小全為立法院長。</p>');
+      $('#slide2-quickview .middle-pic').attr({src: '/images/decision-green.svg'});
+      sParty = 'G';
+      break;
+    case 'O':
+      $('#slide2-quickview .title').html('<p>所有橘黨立委已經在上週開過黨團會議，由於橘黨無人表達角逐院長意願，會中決議開放黨籍立委自主投票。</p>');
+      $('#slide2-quickview .middle-pic').attr({src: '/images/decision-orange.svg'});
+      sParty = 'O';
+      break;
+  }
   showDialogAnimation($('.twr-quick-view'), btn, function(){
 		//show quick view content
-    $('#slide2-quickview .title').html(message);
 	});
 }
 
-function scrollToInfoBox() {
+function scrollToInfoBox(_c) {
   $('.info-box').velocity('scroll', {
-    container: $('#slide1-quickview'),
+    container: $(_c),
     duration: 300,
     easing: 'easeInOutSine'
   });
