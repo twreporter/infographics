510309f510e40bb418b4efc2d1b4af12aed250fb
garfieldduck <garfieldduck@gmail.com>
2016-03-22 12:38:12 +0800

    refine top cover layout

--- steel-slag/app/scripts/main.js
+++ steel-slag/app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  // initiate ScrollMagic
+  let controller = new ScrollMagic.Controller();
+
+  let scene1 = new ScrollMagic.Scene({
+    triggerElement: '#scene-1',
+    triggerHook: 'onLeave',
+    offset: 200
+  })
+  // .setPin("#scene-1")
+  .setVelocity("#scene-1 .blurred-image", {opacity: 1}, {duration: 500, easing: "linear"})
+  .addTo(controller);
+
+});
