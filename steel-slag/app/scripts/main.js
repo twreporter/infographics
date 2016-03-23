7616d6e851e2d5634cef0ef7ff05a3968c960559
garfieldduck <garfieldduck@gmail.com>
2016-03-23 17:19:45 +0800

    added google map with styles

--- steel-slag/app/scripts/main.js
+++ steel-slag/app/scripts/main.js
@@ -5,7 +5,7 @@ $(function() {
   let scene1 = new ScrollMagic.Scene({
     triggerElement: '#scene-1',
     triggerHook: 'onLeave',
-    offset: 200
+    offset: 50
   })
   // .setPin("#scene-1")
   .setVelocity("#scene-1 .blurred-image", {opacity: 1}, {duration: 500, easing: "linear"})
