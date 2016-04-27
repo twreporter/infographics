d0c8dc2113b5cf798ba4fedb263a929329ba2541
garfieldduck <garfieldduck@gmail.com>
2016-04-27 19:11:10 +0800

    added scroll magic slides for the story of the gay family

---
+++ homofamily/app/scripts/infocard.js
@@ -0,0 +1,20 @@
+$(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave'
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("section.scroll-panel");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i])
+				.addTo(controller);
+		}
+	});
