37335dd3a8c82b3a582e57ba1cbc132094f16881
garfieldduck <garfieldduck@gmail.com>
2016-05-04 21:37:17 +0800

    added ending animation

--- homofamily/app/scripts/infocard.js
+++ homofamily/app/scripts/infocard.js
@@ -10,46 +10,53 @@ $(function () { // wait for document ready
 		var slides = document.querySelectorAll("section.scroll-panel");
 
 		// create scene for every slide
-		for (var i=1; i<slides.length-1; i++) {
+		for (var i=1; i<slides.length-2; i++) {
 
 			(function(index) {
 				$('#slide' + (index+1) + ' .content-container').addClass('stay-fixed');
 
 				var sAnimation = new TimelineMax()
-						//  .to("#slide" + (i+1) + " .content-container", 1,  {css:{opacity:0, scale: 0.8}, ease:Quad.easeInOut})
-						//  .to("#slide" + (i+1) + " .content-container", 1,  {css:{opacity:1, scale: 1}, ease:Quad.easeInOut});
-
-						//  .fromTo("#slide" + (index) + " .content-container", 0.5,  {opacity:0.8, scale: 1}, {opacity:0, scale: 0.5, ease:Power4.easeOut},  0)
-							// .fromTo("#slide" + (index) + " .content-container", 0,  {opacity:0, scale: 0.8}, {opacity:0.5, scale: 0.5, ease:Power4.easeOut},  0)
 						 .fromTo("#slide" + (index+1) + " .content-container", 2,  {opacity:0, scale: 0.8}, {opacity:1, scale: 1, ease:Sine.easeIn},  0)
-						 .fromTo("#slide" + (index) + " .content-container", 1,  {opacity:0.3, scale: 0.5}, {opacity:0, scale: 0.5, ease:Power4.easeOut},  0)
-						 	.to("#slide" + (index+1) + " .content-container", 2,  {css:{opacity:1, scale:1}, ease:Quad.easeInOut})
-							.to("#slide" + (index+1) + " .content-container", 2,  {css:{opacity:0.3, scale: 0.5}, ease:Quad.easeInOut})
-							;
+						 .fromTo("#slide" + (index) + " .content-container", 2,  {opacity:0.25, scale: 0.5}, {opacity:0, scale: 0.5, ease:Power4.easeOut},  0)
+						 	.to("#slide" + (index+1) + " .content-container", 3,  {css:{opacity:1, scale:1}, ease:Quad.easeInOut})
+							.to("#slide" + (index+1) + " .content-container", 2,  {css:{opacity:0.25, scale: 0.5}, ease:Quad.easeInOut});
 
 				new ScrollMagic.Scene({
 						triggerElement: "#slide" + (index+1),
-						duration: '250%',
-						triggerHook: 'onLeave',
-						// offset: '-50%'
+						duration: '260%',
+						triggerHook: 'onLeave'
 					})
 					.setPin("#slide" + (index+1))
-					// .setClassToggle("#slide" + (index+1) + " .content-container", "stay-fixed")
 					.setTween(sAnimation)
 					.on('start', function () {
-				    console.log('start ' + "#slide" + (index+1) );
 						// $('#slide' + (index+1) + ' .content-container').removeClass('stay-fixed');
 						$('#slide' + (index+1) + ' .content-container').addClass('content-show');
 				  })
-					.on("end", function() {
-						console.log('end ' + "#slide" + (index+1) );
-						// $('#slide' + (index+1) + ' .content-container').addClass('stay-fixed');
-					})
 					.addTo(controller);
 			}(i));
 
 
 		}
+
+		$('#slide14 .content-container').addClass('stay-fixed');
+		var sAnimation = new TimelineMax()
+				 .fromTo("#slide14" + " .content-container", 2,  {opacity:0, scale: 0.8}, {opacity:1, scale: 1, ease:Sine.easeIn},  0)
+				 .fromTo("#slide13" + " .content-container", 2,  {opacity:0.25, scale: 0.5}, {opacity:0, scale: 0.5, ease:Power4.easeOut},  0)
+					.to("#slide14" + " .content-container", 3,  {css:{opacity:1, scale:1}, ease:Quad.easeInOut})
+					.to("#slide14" + " .content-container .main-img-overlay", 2,  {css:{y: '50%', scale: 1.1}, ease:Quad.easeInOut})
+					.to("#slide14" + " .content-container", 1,  {css:{opacity:0}, ease:Quad.easeInOut});
+
+		new ScrollMagic.Scene({
+				triggerElement: "#slide14",
+				duration: '260%',
+				triggerHook: 'onLeave'
+			})
+			.setPin("#slide-end")
+			.setTween(sAnimation)
+			.on('start', function () {
+				$('#slide14' + ' .content-container').addClass('content-show');
+			})
+			.addTo(controller);
 
 		// for (var i=0; i<slides.length-1; i++) {
 		// 	new ScrollMagic.Scene({
