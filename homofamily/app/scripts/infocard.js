3bffe370f08fd1da2bfbf3bf5679fc40919f3640
garfieldduck <garfieldduck@gmail.com>
2016-05-04 17:04:39 +0800

    update animation and content

--- homofamily/app/scripts/infocard.js
+++ homofamily/app/scripts/infocard.js
@@ -11,44 +11,63 @@ $(function () { // wait for document ready
 
 		// create scene for every slide
 		for (var i=1; i<slides.length-1; i++) {
-			var sAnimation = new TimelineMax()
-					//  .from("#slide" + (i+1) + " .content-container", 1,  {css:{opacity:0, scale: 0.5}, ease:Quad.easeInOut})
-	         .fromTo("#slide" + (i+1) + " .content-container", 1,  {opacity:0, scale: 0.8}, {opacity:1, scale: 1, ease:Quad.easeOut},  0)
-					//  .to("#slide" + (i+1) + " .content-container", 1,  {css:{opacity:0.5, scale: 0.8}, ease:Quad.easeInOut});
 
-			new ScrollMagic.Scene({
-					triggerElement: "#slide" + (i+1),
-					// duration: '200%',
-					triggerHook: 'onLeave',
-					offset: '50%'
-				})
-				.setPin("#slide" + (i+1))
-				.setClassToggle("#slide" + (i+1) + " .content-container", "stay-fixed")
-				.setTween(sAnimation)
-				.addTo(controller);
+			(function(index) {
+				var sAnimation = new TimelineMax()
+						//  .to("#slide" + (i+1) + " .content-container", 1,  {css:{opacity:0, scale: 0.8}, ease:Quad.easeInOut})
+						//  .to("#slide" + (i+1) + " .content-container", 1,  {css:{opacity:1, scale: 1}, ease:Quad.easeInOut});
+						 .fromTo("#slide" + (index) + " .content-container", 1,  {opacity:1, scale: 1}, {opacity:0, scale: 0.5, ease:Power4.easeOut},  0)
+		         .fromTo("#slide" + (index+1) + " .content-container", 1,  {opacity:0, scale: 0.8}, {opacity:1, scale: 1, ease:Sine.easeIn},  0)
+							.to("#slide" + (index+1) + " .content-container", 1,  {css:{opacity:1, scale: 1}, ease:Quad.easeInOut})
+							;
+
+				new ScrollMagic.Scene({
+						triggerElement: "#slide" + (index+1),
+						duration: '120%',
+						triggerHook: 'onLeave',
+						// offset: '-10%',
+					})
+					.setPin("#slide" + (index+1))
+					// .setClassToggle("#slide" + (index+1) + " .content-container", "stay-fixed")
+					.setTween(sAnimation)
+					.on('start', function () {
+				    console.log('start ' + "#slide" + (index+1) );
+						// $('#slide' + (index+1) + ' .content-container').removeClass('stay-fixed');
+						$('#slide' + (index+1) + ' .content-container').addClass('stay-fixed');
+				  })
+					.on("end", function() {
+						console.log('end ' + "#slide" + (index+1) );
+						$('#slide' + (index+1) + ' .content-container').addClass('stay-fixed');
+					})
+					.addTo(controller);
+			}(i));
+
+
 		}
 
 		// for (var i=0; i<slides.length-1; i++) {
 		// 	new ScrollMagic.Scene({
-		// 			triggerElement: "#slide" + (i+1) + " .content-container",
-		// 			// duration: '120%',
+		// 			triggerElement: "#slide" + (i+1),
+		// 			duration: '150%',
 		// 			triggerHook: 'onLeave',
-		// 			offset: '50%'
+		// 			// offset: '50%'
 		// 		})
 		// 		// .setPin("#slide" + (i+1) + " .content-container")
-		// 	  .setClassToggle("#slide" + (i+1) + " .content-container", "stayFixed")
+		// 	  .setPin("#slide" + (i+1))
 		// 		.addTo(controller);
 		// }
 
-		var sAnimation1 = new TimelineMax()
-         .to("#slide1 > img", 0.5,  {css:{scale:0.05, opacity:0, rotation: 180}, ease:Quad.easeInOut});
+	// 	var sAnimation1 = new TimelineMax()
+  //        .to("#slide1 > img", 0.5,  {css:{scale:0.05, opacity:0, rotation: 180}, ease:Quad.easeInOut});
+	//
+	// 	new ScrollMagic.Scene({
+	// 			triggerElement: "#slide1",
+	// 			triggerHook: 'onEnter'
+	// 		})
+	// 		.setPin("#slide1")
+	// 		.setTween(sAnimation1)
+	// 		.setClassToggle("#slide1 .content-container", "stay-fixed")
+	// 		.addTo(controller);
 
-		new ScrollMagic.Scene({
-				triggerElement: "#slide1",
-				triggerHook: 'onEnter'
-			})
-			.setPin("#slide1")
-			.setTween(sAnimation1)
-			.setClassToggle("#slide1 .content-container", "stay-fixed")
-			.addTo(controller);
+
 	});
--- homofamily/app/scripts/main.js
+++ homofamily/app/scripts/main.js
@@ -25,7 +25,23 @@ function setMpFilterAnimation(controller, shuffle, scene, filterItem) {
   });
 }
 
-// $( document ).ready(function() {
+function enbaleSmoothScroll() {
+  $('a[href*="#chapter"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+}
+
+$( document ).ready(function() {
+  enbaleSmoothScroll();
 //   let Shuffle = window.shuffle;
 //   let element = document.getElementById('shuffle-mp-figure');
 //
@@ -48,4 +64,4 @@ function setMpFilterAnimation(controller, shuffle, scene, filterItem) {
 //   // for(let i=0; i<MP_LIST.length; i++){
 //   //   setMpFilterAnimation(controller, sff, MP_LIST[i].scene, MP_LIST[i].key);
 //   // }
-// });
+});
