22baf6085cf64301ce1f352d92430d5d184fd7c7
ChienHsin-Chan <hcchien@gmail.com>
2016-05-24 14:24:04 +0800

    update the data structure for the years

--- chinfeng-chen/fetchPlayer.py
+++ chinfeng-chen/fetchPlayer.py
@@ -16,10 +16,11 @@ buffer = StringIO()
 c = pycurl.Curl()
 cc = pycurl.Curl()
 players = {}
+players['year'] = {}
 players['total'] = 0
 
 for year in range(1876, 2003):
-    players[year] = {}
+    players['year'][year] = {}
     url = 'http://www.baseball-almanac.com/players/birthplace.php?y='+str(year)
     c.setopt(c.URL, url)
     c.setopt(c.WRITEDATA, buffer)
@@ -28,12 +29,12 @@ for year in range(1876, 2003):
 
     us = re.search("<table border='0' align='center'><tr><td colspan='4' align='left'><font face='Arial' size='-1'><b>United States \((\d+)\)<br>&nbsp;</b></font></td></tr>", result)
     if (us):
-        players[year]['USA'] = us.group(1)
+        players['year'][year]['USA'] = us.group(1)
         players['total'] += int(us.group(1))
 
     others = re.findall("<td><a href='/players/birthplace\.php\?loc=\w+&y=\d+'><font face='Arial' size='-1'>(\w+)</font></a><font face='Arial' size='-1'>&nbsp;\((\d+)\)</font></td>", result)
     for country in others:
-        players[year][country[0]] = country[1]
+        players['year'][year][country[0]] = country[1]
         players['total'] += int(country[1])
 
 
