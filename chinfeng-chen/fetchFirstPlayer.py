d406c2e77ea928bb0d3e17c02059c091a9ceedff
ChienHsin-Chan <hcchien@gmail.com>
2016-07-04 18:07:01 +0800

    add the count to the nation first player

--- chinfeng-chen/fetchFirstPlayer.py
+++ chinfeng-chen/fetchFirstPlayer.py
@@ -18,6 +18,7 @@ cc = pycurl.Curl()
 players = {}
 players['year'] = {}
 players['nation'] = {}
+count = 0
 
 url = 'https://en.wikipedia.org/wiki/List_of_countries_with_their_first_Major_League_Baseball_player'
 c.setopt(c.URL, url)
@@ -27,9 +28,10 @@ result = buffer.getvalue()
 
 nationList = re.findall("<td>(\w+?)</td>\n<td><a href=\"/wiki/.+?\" title=\".+?\">([\w ]+?)</a></td>\n<td>(\d{4})<sup id=\".+?\" class=\"reference\">", result)
 for nation in nationList:
+    count += 1
     players['year'][nation[2]] = {"nation": nation[0], "player": nation[1]}
     players['nation'][nation[0]] = {"year": nation[2], "player": nation[1]}
 
-
+players['count'] = count
 print json.dumps(players)
 
