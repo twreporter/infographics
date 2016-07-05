ccb529c6f385ee87aa5e5a94cb5ef4758042cb89
Mika <garfieldduck@gmail.com>
2016-07-05 11:37:05 +0800

    Refine the 3D panorama viewer
    - fix the width and height issue to fit the iframe box
    - remove the control bar

--- film-center/app/scripts/pano-viewer.js
+++ film-center/app/scripts/pano-viewer.js
@@ -1,20 +1,26 @@
 console.log('\'Allo \'Allo!');
 
-var panorama, viewer;
+let panorama = new PANOLENS.ImagePanorama('images/IMG_5818-Panorama-Featured-1024x512.jpg');  // test images
 
-        panorama = new PANOLENS.ImagePanorama( 'images/IMG_5818-Panorama-Featured-1024x512.jpg' );  // test images
+let infospot = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+infospot.position.set(3957.4, -744.48, 2956.61);
+infospot.addHoverText('Test Info Spot');
 
-        viewer = new PANOLENS.Viewer({
-                    controlBar: true,           // Vsibility of bottom control bar
-                    controlButtons: [],         // Buttons array in the control bar. Default to ['fullscreen', 'navigation', 'vr', 'video']
-                    autoHideControlBar: false,  // Auto hide control bar
-                    autoHideInfospot: true,     // Auto hide infospots
-                    horizontalView: false,      // Allow only horizontal camera control
-                    cameraFov: 60,              // Camera field of view in degree
-                    reverseDragging: false,     // Reverse orbit control direction
-                    enableReticle: false,       // Enable reticle for mouseless interaction
-                    dwellTime: 1500,            // Dwell time for reticle selection in millisecond
-                    autoReticleSelect: true,    // Auto select a clickable target after dwellTime
-                    passiveRendering: false,    // Render only when control triggered by user input 
-                });
-        viewer.add( panorama );
+panorama.add(infospot);
+
+let viewer = new PANOLENS.Viewer({
+        container: document.body,   // A DOM Element container. Default: document.body
+        controlBar: false,           // Vsibility of bottom control bar
+        controlButtons: [],         // Buttons array in the control bar. Default to ['fullscreen', 'navigation', 'vr', 'video']
+        autoHideControlBar: true,  // Auto hide control bar
+        autoHideInfospot: true,     // Auto hide infospots
+        horizontalView: false,      // Allow only horizontal camera control
+        cameraFov: 60,              // Camera field of view in degree
+        reverseDragging: false,     // Reverse orbit control direction
+        enableReticle: false,       // Enable reticle for mouseless interaction
+        dwellTime: 1500,            // Dwell time for reticle selection in millisecond
+        autoReticleSelect: true,    // Auto select a clickable target after dwellTime
+        passiveRendering: false,    // Render only when control triggered by user input 
+});
+
+viewer.add(panorama);
