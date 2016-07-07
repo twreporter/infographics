b7fe93d1e0724ffef61266df00d268cb5f64904e
Mika <garfieldduck@gmail.com>
2016-07-07 19:08:57 +0800

    Add EaseJS to control png image sprites
    Add EaseJS to control png image sprites

--- film-center/app/scripts/main.js
+++ film-center/app/scripts/main.js
@@ -1,1 +1,32 @@
 console.log('\'Allo \'Allo!');
+
+
+var stage = new createjs.Stage('myCanvas');
+var shape = new createjs.Shape();
+shape.graphics.beginFill('red').drawRect(0, 0, 1000, 563);
+stage.addChild(shape);
+stage.update();
+
+var ss = new createjs.SpriteSheet({
+    frames: {
+        width: 1000,
+        height: 563,
+        numFrames: 12
+    },
+    animations: {
+        run: [0, 50],
+        jump: [12, 0, 'run']
+    },
+    images: ['./images/myvideo.jpg']
+});
+
+var sprite = new createjs.Sprite(ss);
+sprite.scaleY = sprite.scaleX = 0.2;
+stage.addChild(sprite);
+sprite.gotoAndPlay('run');
+
+sprite.on('click', function() {
+    sprite.gotoAndPlay('run');
+});
+
+createjs.Ticker.on('tick', stage);
