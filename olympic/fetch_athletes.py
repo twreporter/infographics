91ff26fa4ba371b437706839eb4c4e8feaff9135
ChienHsin-Chan <hcchien@gmail.com>
2016-07-14 19:21:14 +0800

    update the fetch program to save the json file

--- olympic/fetch_athletes.py
+++ olympic/fetch_athletes.py
@@ -12,10 +12,10 @@ from StringIO import StringIO
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-list = {}
+full_list = {}
 
-#for year in [2016, 2012, 2008, 2004, 2000, 1996, 1992, 1988, 1984]:
-for year in [2016, 2012]:
+for year in [2016, 2012, 2008, 2004, 2000, 1996, 1992, 1988, 1984]:
+#for year in [2016, 2012]:
     print year
     wiki_url = "https://en.wikipedia.org/wiki/"+str(year)+"_Summer_Olympics"
     buffer = StringIO()
@@ -25,12 +25,12 @@ for year in [2016, 2012]:
     c.perform()
     result = buffer.getvalue()
     c.close()
-    list[year] = {}
+    full_list[year] = {}
     nations = re.findall("<li><img .+?<a href=\"(.+?)\" title=\"\w+? at the \d+ Summer Olympics\">(.+?)</a>.+?\((\d+)\)</span></li>", result)
     for nation in nations:
-        print nation
+        print "\t" + str(nation[1])
         nation_buffer = StringIO()
-        list[year][nation[1]] = { 'url': nation[0], 'total': nation[2], 'items': {}}
+        full_list[year][nation[1]] = { 'url': nation[0], 'total': nation[2], 'items': {}}
         c = pycurl.Curl()
         c.setopt(c.URL, "https://en.wikipedia.org" + nation[0])
         c.setopt(c.WRITEDATA, nation_buffer)
@@ -44,31 +44,34 @@ for year in [2016, 2012]:
                 break
             athletes = re.match(r"^(\w+?)</span>.+?</h2>(.+)$", item, re.DOTALL)
             if athletes:
-                list[year][nation[1]]['items'][athletes.group(1)] = []
-                #athlete = re.findall("<td align=\"left\".*?><a href=\"[\w\d\/\_\?:=;&%\(\)]+?\".*?title=\".+?\">([a-zA-Z\_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>", athletes.group(2), re.DOTALL)
-                athlete = re.findall("<td align=\"left\".*?><a href=\"[\w\d\/\_\?:=;&%\(\)]+?\".*?title=\".+?\">([\w\_ ]+?)</a>", athletes.group(2), re.UNICODE)
+                full_list[year][nation[1]]['items'][athletes.group(1)] = []
+                # for the table type
+                athlete = re.findall("(<tr.*?>.*?<td align=\"left\".*?><a href=\".+?\".+?title=\".+? \(page does not exist\)\">([a-zA-Z_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>.+?<\/td>(.+?)<\/tr>|<tr.*?>.+?<td align=\"left\".*?><a href=\"[\w\d\/\_\?:=;&%\(\)]+?\".*?title=\".+?\">([a-zA-Z\_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>.+?<\/td>(.+?)<\/tr>)", athletes.group(2), re.DOTALL)
                 if athlete:
                     for name in athlete:
-                        if (name not in list[year][nation[1]]['items'][athletes.group(1)]):
-                            list[year][nation[1]]['items'][athletes.group(1)].append(name)
-                #athlete = re.findall("<td align=\"left\".*?><a href=\".+?\".+?title=\".+? \(page does not exist\)\">([a-zA-Z_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>", athletes.group(2), re.DOTALL)
-                athlete = re.findall("<td align=\"left\".*?><a href=\"[\w\d\/\_\?:=;&%\(\)]+?\".*?title=\".+?\">([\w\_ ]+?)</a>", athletes.group(2), re.UNICODE)
-                if athlete:
-                    for name in athlete:
-                        if (name not in list[year][nation[1]]['items'][athletes.group(1)]):
-                            list[year][nation[1]]['items'][athletes.group(1)].append(name)
+                        gender = re.match(".*?(Men|Women).*?", name[0], re.DOTALL)
+                        if (gender != None):
+                            sex = gender.group(1)
+                        else:
+                            sex = "N/A"
+                        athlete_name = name[1] or name[3]
+                        if (athlete_name not in full_list[year][nation[1]]['items'][athletes.group(1)]):
+                            full_list[year][nation[1]]['items'][athletes.group(1)].append({"name": athlete_name, "gender": sex})
+                # for the listing type    
                 list_style = re.findall("<li>(.+?)<\/li>", athletes.group(2), re.DOTALL)
                 if (list_style):
                     for list_item in list_style:
-                        #athletes_list = re.findall("<a href=\".+?\".*?title=\".+? \(page does not exist\)\">([a-zA-Z_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>", list_item, re.DOTALL)
-                        athletes_list = re.findall("<td align=\"left\".*?><a href=\"[\w\d\/\_\?:=;&%\(\)]+?\".*?title=\".+?\">([\w\_ ]+?)</a>", list_item, re.UNICODE)
+                        gender = re.match(".*?(Men|Women).*?", list_item[0], re.DOTALL)
+                        if (gender != None):
+                            sex = gender.group(1)
+                        else:
+                            sex = "N/A"
+                        athletes_list = re.findall("(<a href=\".+?\".*?title=\".+? \(page does not exist\)\">([a-zA-Z_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>|<a href=\".+?\".*?title=\".+?\">([a-zA-Z_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>)", list_item, re.DOTALL)
                         for name in athletes_list:
-                            if (name not in list[year][nation[1]]['items'][athletes.group(1)]):
-                                list[year][nation[1]]['items'][athletes.group(1)].append(name)
-                        #athletes_list = re.findall("<a href=\".+?\".*?title=\".+?\">([a-zA-Z_ ÓÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿıŒœŠšŸŽž]+?)</a>", list_item, re.DOTALL)
-                        athletes_list = re.findall("<td align=\"left\".*?><a href=\"[\w\d\/\_\?:=;&%\(\)]+?\".*?title=\".+?\">([\w\_ ]+?)</a>", list_item, re.UNICODE)
-                        for name in athletes_list:
-                            if (name not in list[year][nation[1]]['items'][athletes.group(1)]):
-                                list[year][nation[1]]['items'][athletes.group(1)].append(name)
-        print list[year][nation[1]]['items']
+                            athlete_name = name[1] or name[2]
+                            if (athlete_name not in full_list[year][nation[1]]['items'][athletes.group(1)]):
+                                full_list[year][nation[1]]['items'][athletes.group(1)].append({"name": athlete_name, "gender": sex})
+
+f = open('athletes_full.json', w)
+f.write(json.dump(full_list))
 
