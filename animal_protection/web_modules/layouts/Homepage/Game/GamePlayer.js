eed224fd655b68c722ed81c3cb3a5e46eefc8a9f
Mika <garfieldduck@gmail.com>
2016-10-17 17:41:02 +0800

    update desktop game container
    update desktop game container

--- animal_protection/web_modules/layouts/Homepage/Game/GamePlayer.js
+++ animal_protection/web_modules/layouts/Homepage/Game/GamePlayer.js
@@ -6,6 +6,8 @@ import classnames from "classnames"
 import styles from "./GamePlayer.scss"
 import commonStyles from "../../../styles/common.scss"
 
+import dogAnimated from "../../../../content/assets/dog_animated.gif"
+// import dogCaptured from "../../../../content/assets/dog_captured.gif"
 import gameIcon from "../../../../content/assets/game_cnt_icon.svg"
 
 let velocity
@@ -32,33 +34,53 @@ let GameFooter = (props) => {
   )
 }
 
+const Dog = () => {
+  return (
+    <div className={ styles["dog"] }>
+      <img src={ dogAnimated } />
+    </div>
+  )
+}
+
 export default class GamePlayer extends Component {
   constructor(props) {
     super(props)
     this.state = {
       isIntoHidden: true,
+      isOpened: false,
       isClosing: false,
     }
     this.handleClose = this.handleClose.bind(this)
+    this._handleDialogOpened = this._handleDialogOpened.bind(this)
   }
 
   componentDidMount() {
     const { outerWidth, outerHeight, outerTop } = this.props
     velocity(this.container, { width: [ "100%", outerWidth ], height: [ "100%", outerHeight ],
       top: [ 0, outerTop ], translateX: [ "-50%", "-50%" ] }, { duration: 1000, easing: "easeInOutSine" })
+      .then(this._handleDialogOpened())
+  }
+
+  _handleDialogOpened() {
+    this.setState({ isOpened: true })
   }
 
   handleClose() {
     const { outerWidth, outerHeight, outerTop } = this.props
     this.setState({ isClosing: true })
-    velocity(this.container, { width: outerWidth, height: outerHeight, top: outerTop }, 1000)
+    velocity(this.container, { width: outerWidth, height: outerHeight, top: outerTop }, { duration: 1000, easing: "easeInOutSine" })
       .then(() => this.props.onClose())
   }
 
   render() {
-    const { isIntoHidden, isClosing } = this.state
+    const { isIntoHidden, isOpened, isClosing } = this.state
     const introClass = isIntoHidden ? styles["hide"] : null
     const closingClass = isClosing ? styles["hide-fast"] : null
+
+    const gameContainer = isOpened ?
+      <div className={ styles["game-container"] }>
+        <Dog />
+      </div> : null
 
     return (
       <div className={ classnames(styles.container) }
@@ -74,6 +96,10 @@ export default class GamePlayer extends Component {
               <div className={ classnames(commonStyles["img-responsive"], commonStyles["overlay-svg"], styles["center-dog"]) }
                 dangerouslySetInnerHTML={ { __html: gameIcon } } />
             </div>
+          </div>
+
+          <div className={ styles["game-outer"] }>
+            { gameContainer }
           </div>
 
           <GameFooter num={ 50 } />
