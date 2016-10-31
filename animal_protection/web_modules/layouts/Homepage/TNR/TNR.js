c24175339b88581d0a37e181f98f56695b3d03a0
Mika <garfieldduck@gmail.com>
2016-10-31 15:48:48 +0800

    Finish section 2-2
    Finish section 2-2

--- animal_protection/web_modules/layouts/Homepage/TNR/TNR.js
+++ animal_protection/web_modules/layouts/Homepage/TNR/TNR.js
@@ -17,8 +17,13 @@ import tnrN from "../../../../content/assets/tnvr-n2.svg"
 import tnrV from "../../../../content/assets/tnvr-v.svg"
 import tnrR from "../../../../content/assets/tnvr-r.svg"
 import rInfoImg from "../../../../content/assets/info-box-icon.svg"
+import problem1 from "../../../../content/assets/traffic-accident.svg"
+import problem2 from "../../../../content/assets/cold.svg"
+import problem3 from "../../../../content/assets/poison.svg"
+import problem4 from "../../../../content/assets/wild-animal.svg"
 
-import { chapter, topBox, titles, research, tnvrStrings } from "./text"
+import { chapter, topBox, titles, research, tnvrStrings,
+  sec2Des, sec3Des, sec4Des, problemStrings } from "./text"
 
 const SLIDEIN_EFFECT = { translateX: [ "-50%", "-50%" ], translateY: [ "0%", "60%" ], opacity: [ 1, 0.5 ] }
 
@@ -38,6 +43,10 @@ export default class Tnr extends Component {
     this._handleAnimationN = this._handleAnimationN.bind(this)
     this._handleAnimationV = this._handleAnimationV.bind(this)
     this._handleAnimationR = this._handleAnimationR.bind(this)
+    this._handleProblem1 = this._handleProblem1.bind(this)
+    this._handleProblem2 = this._handleProblem2.bind(this)
+    this._handleProblem3 = this._handleProblem3.bind(this)
+    this._handleProblem4 = this._handleProblem4.bind(this)
   }
 
   _handleAnimationT() {
@@ -51,7 +60,6 @@ export default class Tnr extends Component {
       }
     }
     catch (e) {
-      console.log("this.cTText", this.cTText)
       console.log("error playing animations")
     }
   }
@@ -98,6 +106,68 @@ export default class Tnr extends Component {
         velocity(tWow, { translateX: [ 0, -20 ], scale: [ 1, 1.2 ], opacity: [ 1, 0.2 ] }, { delay: 650, duration: 800 })
         velocity(tHand, { translateY: [ 0, 10 ], translateX: [ 0, -20 ] }, { delay: 650, duration: 1000 })
         velocity(this.cRText, SLIDEIN_EFFECT, { delay: 650, duration: 1000 })
+      }
+    }
+    catch (e) {
+      console.log("error playing animations")
+    }
+  }
+
+  _handleProblem1() {
+    try {
+      const tLeft = document.getElementById("p1-dog")
+      const tRight = document.getElementById("p1-wow")
+      if (tLeft && tRight) {
+        velocity(tLeft, { translateX: [ 0, -50 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 900 })
+        velocity(tRight, { scale: [ 1, 1.1 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 1000 })
+        velocity(this.p1Text, SLIDEIN_EFFECT, { delay: 300, duration: 1000 })
+      }
+    }
+    catch (e) {
+      console.log("error playing animations")
+    }
+  }
+
+  _handleProblem2() {
+    try {
+      const tLeft = document.getElementById("p2-dog")
+      const tRight = document.getElementById("p2-wind")
+      if (tLeft && tRight) {
+        velocity(tLeft, { translateY: [ 0, 30 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 900 })
+        velocity(tRight, { scale: [ 1, 1.2 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 1000 })
+        velocity(this.p2Text, SLIDEIN_EFFECT, { delay: 300, duration: 1000 })
+      }
+    }
+    catch (e) {
+      console.log("error playing animations")
+    }
+  }
+
+  _handleProblem3() {
+    try {
+      const tLeft = document.getElementById("p3-dog")
+      const tRight = document.getElementById("p3-poison_food")
+      if (tLeft && tRight) {
+        velocity(tLeft, { translateX: [ 0, -50 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 900 })
+        velocity(tRight, { scale: [ 1, 1.1 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 1000 })
+        velocity(this.p3Text, SLIDEIN_EFFECT, { delay: 300, duration: 1000 })
+      }
+    }
+    catch (e) {
+      console.log("error playing animations")
+    }
+  }
+
+  _handleProblem4() {
+    try {
+      const tLeft = document.getElementById("p4-bird")
+      const tRight = document.getElementById("p4-pangolin")
+      const tTop = document.getElementById("p4-dog")
+      if (tLeft && tRight) {
+        velocity(tLeft, { translateX: [ 0, -50 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 900 })
+        velocity(tRight, { scale: [ 1, 1.1 ], opacity: [ 1, 0.8 ] }, { delay: 300, duration: 1000 })
+        velocity(tTop, { scale: [ 1, 1.1 ], translateY: [ 0, -20 ] }, { delay: 300, duration: 1000 })
+        velocity(this.p4Text, SLIDEIN_EFFECT, { delay: 300, duration: 1000 })
       }
     }
     catch (e) {
@@ -159,21 +229,68 @@ export default class Tnr extends Component {
                   </VisibleSensor>
                 </div>
               </div>
-
             </Subsection>
             <Subsection curSec={ 2 } titles={ titles } subIndex={ 1 }>
-              this is a subsection
-              <div></div>
-            </Subsection>
-            <Game />
-            <div className={ classnames(styles["research-des"]) }>
-              <div className={ classnames(styles["title-box"]) }>
-                <div className={ classnames(styles["yoyo"], commonStyles["overlay-svg"]) }
-                  dangerouslySetInnerHTML={ { __html: rInfoImg } } />
-                <div className={ styles["r-title"] }>{ research.title }</div>
+              <Markdown className={ commonStyles["inner-text"] } source={ sec2Des } />
+              <div className={ commonStyles["wrap-grids"] }>
+                <div className={ classnames(commonStyles["grid-50"], commonStyles["c-grid"]) }>
+                  <VisibleSensor handleVisible={ this._handleProblem1 }>
+                    <div className={ classnames(commonStyles["img-responsive"],
+                      styles["yoyo"], commonStyles["overlay-svg"]) }
+                      style={ { marginTop: "-12%", marginLeft: "-10%", width: "120%", maxWidth:"120%" } }
+                      dangerouslySetInnerHTML={ { __html: problem1 } }
+                      ref={ (ref) => this.p1G = ref }
+                    />
+                  <p className={ commonStyles["g-btm-text"] } style={ { marginBottom: "-12%" } } ref={ (ref) => this.p1Text = ref }>{ problemStrings[0] }</p>
+                  </VisibleSensor>
+                </div>
+                <div className={ classnames(commonStyles["grid-50"], commonStyles["c-grid"]) }>
+                  <VisibleSensor handleVisible={ this._handleProblem2 }>
+                    <div className={ classnames(commonStyles["img-responsive"],
+                      styles["yoyo"], commonStyles["overlay-svg"]) }
+                      dangerouslySetInnerHTML={ { __html: problem2 } }
+                      ref={ (ref) => this.p2G = ref }
+                    />
+                  <p className={ commonStyles["g-btm-text"] } ref={ (ref) => this.p2Text = ref }>{ problemStrings[1] }</p>
+                  </VisibleSensor>
+                </div>
+                <div className={ classnames(commonStyles["grid-50"], commonStyles["c-grid"]) }>
+                  <VisibleSensor handleVisible={ this._handleProblem3 }>
+                    <div className={ classnames(commonStyles["img-responsive"],
+                      styles["yoyo"], commonStyles["overlay-svg"]) }
+                      dangerouslySetInnerHTML={ { __html: problem3 } }
+                      ref={ (ref) => this.p3G = ref }
+                    />
+                  <p className={ commonStyles["g-btm-text"] } ref={ (ref) => this.p3Text = ref }>{ problemStrings[2] }</p>
+                  </VisibleSensor>
+                </div>
+                <div className={ classnames(commonStyles["grid-50"], commonStyles["c-grid"]) }>
+                  <VisibleSensor handleVisible={ this._handleProblem4 }>
+                    <div className={ classnames(commonStyles["img-responsive"],
+                      styles["yoyo"], commonStyles["overlay-svg"]) }
+                      dangerouslySetInnerHTML={ { __html: problem4 } }
+                      ref={ (ref) => this.p4G = ref }
+                    />
+                  <p className={ commonStyles["g-btm-text"] } ref={ (ref) => this.p4Text = ref }>{ problemStrings[3] }</p>
+                  </VisibleSensor>
+                </div>
               </div>
-              <Markdown source={ research.des } />
-            </div>
+            </Subsection>
+            <Subsection curSec={ 3 } titles={ titles } subIndex={ 2 }>
+              <Markdown className={ commonStyles["inner-text"] } source={ sec3Des } />
+            </Subsection>
+            <Subsection curSec={ 4 } titles={ titles } subIndex={ 3 }>
+              <Markdown className={ commonStyles["inner-text"] } source={ sec4Des } />
+              <Game />
+              <div className={ classnames(styles["research-des"]) }>
+                <div className={ classnames(styles["title-box"]) }>
+                  <div className={ classnames(styles["yoyo"], commonStyles["overlay-svg"]) }
+                    dangerouslySetInnerHTML={ { __html: rInfoImg } } />
+                  <div className={ styles["r-title"] }>{ research.title }</div>
+                </div>
+                <Markdown source={ research.des } />
+              </div>
+            </Subsection>
           </div>
         </div>
       </div>
